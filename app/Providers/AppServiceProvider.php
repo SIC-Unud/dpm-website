0258988d8782ee963d06cdb12d52e2217bb7db4f
Wahyu Pranata <wahyupranata131@gmail.com>
2024-06-25 18:40:32 +0800

    Update app environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,9 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') == 'production') {
-            URL::forceScheme('https');
-        }
         Paginator::useBootstrap();
         Validator::extend('recaptcha', 'App\Rules\ReCaptcha@passes');
     }
